e7bd8a32a32029b910bbd44b711e70c4aa984752
Beyonce Leonard <beyonceleonard65@gmail.com>
2023-06-14 15:44:26 +0200

    second

--- src/components/MyMapComp.vue
+++ src/components/MyMapComp.vue
@@ -6,7 +6,8 @@
 
 <script>
 import L from 'leaflet';
-// import vechicleCoordinates from '../data/vechicleCoordinates';
+import vechicleCoordinates from '../data/vechicleCoordinates';
+// import taxicab from '../assets/taxicab.png';
 export default {
     mounted(){
         const map = L.map('map').fitWorld();
@@ -25,13 +26,12 @@ function onLocationError(e) {
     alert(e.message);
 }
 map.on('locationerror', onLocationError);
-// for(const vechicleCoordinate of vechicleCoordinates)
-// L.marker([vechicleCoordinate.latitude,vechicleCoordinate.longitude],{icon: taxiIcon}).addTo(map);
-
-//  var taxiIcon = L.icon({
-//     iconUrl: '../assets/taxicab.png',
-//     iconSize: [50,50]
-//  })
+ var taxiIcon = L.icon({
+    iconUrl: 'taxicab.png',
+    iconSize: [50,50]
+ })
+ for(const vechicleCoordinate of vechicleCoordinates)
+ L.marker([vechicleCoordinate.latitude,vechicleCoordinate.longitude],{icon: taxiIcon}).addTo(map);
     }
 }
 
